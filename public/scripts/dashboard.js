a1d8fc1ac4dff1a9d958ed4eccc61772090f185b
Simon Kaufmann <simon.k1996@gmail.com>
2020-03-07 16:00:03 -0800

    Browsers Chart works

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -15,6 +15,24 @@ function refreshData() {
     .then(json => {
       render(json);
     });
+
+
+var chartData = {
+  // Specify your chart type
+  "type": "bar",
+  // Add your series data
+  "series": [
+    { "values": [35, 42, 67, 89] },
+    { "values": [28, 40, 39, 36] }
+  ]
+};
+// Render your chart [3]
+zingchart.render({
+  id:'firstchart',
+  data:chartData,
+  height:400,
+  width:600
+});
 }
 
 function render(json) {
@@ -41,14 +59,105 @@ function getSitesArray(db) {
   return list;
 }
 
+function getBrowser(userAgent) {
+  // Inspired by code from https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser 
+  if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+  {
+      return 'Opera';
+  }
+  else if(navigator.userAgent.indexOf("Chrome") != -1 )
+  {
+      return 'Chrome';
+  }
+  else if(navigator.userAgent.indexOf("Safari") != -1)
+  {
+      return 'Safari';
+  }
+  else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+  {
+      return 'Firefox';
+  }
+  else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+  {
+    return 'IE';
+  }  
+  else 
+  {
+    return 'other';
+  }
+}
+
 function getBrowserData(dbObject) {
   let pages = getSitesArray(dbObject);
   
   let agents = [];
 
-  pages.forEach((page) => {
-    agents.push(page.userAgent);
+  let browsers = pages.map(element => element.userAgent)
+                      .map(getBrowser);
+
+  let browserCount = {};
+
+  browsers.forEach((browser) => {
+    if (!browserCount.hasOwnProperty(browser)) {
+      browserCount[browser] = 1;
+    } else {
+      browserCount[browser] += 1;
+    }
   });
 
+  renderBrowserChart(browserCount);
+}
+
+function renderBrowserChart(browserCount) {
+  let colors = ['#50ADF5','#FF7965','#FFCB45','#6877e5','#6FB07F'];
+  let colorIndex = 0;
+
+  let series = [];
+
+  Object.keys(browserCount).forEach((key) => {
+    series.push({
+      values : [browserCount[key]],
+      text: key,
+      backgroundColor: colors[(colorIndex++)%colors.length]
+    });
+  })
+
+  let browserChartConfig = {
+    type: "pie", 
+    plot: {
+      borderColor: "#2B313B",
+      borderWidth: 5,
+      // slice: 90,
+      valueBox: {
+        placement: 'out',
+        text: '%t\n%npv%',
+        fontFamily: "Open Sans"
+      },
+      tooltip:{
+        fontSize: '18',
+        fontFamily: "Open Sans",
+        padding: "5 10",
+        text: "%npv%"
+      }
+    },
+    title: {
+      fontColor: "#8e99a9",
+      text: 'Page Loads by Browser',
+      align: "left",
+      offsetX: 10,
+      //fontFamily: "Sans",
+      fontSize: 20
+    },
+    plotarea: {
+      margin: "20 0 0 0"  
+    },
+    series : series
+ };
   
+ zingchart.render({ 
+   id : 'browser-chart', 
+   data : browserChartConfig, 
+   height: 400, 
+   width: 600 
+ });
 }
